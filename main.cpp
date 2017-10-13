ebb847f22534511f9cfe7d117e146f68c0a50fed
Mohan <7244220+mohanliu@users.noreply.github.com>
2017-10-13 17:46:07 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// Fig. 8.27: fig08_27.cpp
-// Card shuffling and dealing program.
 #include "DeckOfCards.h" // DeckOfCards class definition
 
 #include <iostream>
@@ -55,19 +53,3 @@ int main()
     
     return 0; // indicates successful termination
 } // end main
-
-
-/**************************************************************************
- * (C) Copyright 1992-2005 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- **************************************************************************/
