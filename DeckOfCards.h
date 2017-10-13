5e24f43b085da4e5562490400c0e795f701c5a8b
Mohan Liu <mohan.liu.6@gmail.com>
2017-10-13 18:05:17 -0500

    Initial commit

---
+++ DeckOfCards.h
@@ -0,0 +1,11 @@
+// DeckOfCards class definition
+class DeckOfCards
+{
+public:
+    DeckOfCards(); // constructor initializes deck
+    void shuffle(); // shuffles cards in deck
+    void deal(int); // deals cards in deck
+    int * fivehand(int, int);
+private:
+    int deck[ 4 ][ 13 ]; // represents deck of cards
+}; // end class DeckOfCards
